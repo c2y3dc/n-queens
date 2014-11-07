039bd5347b6e53fb9876e872abfadd92e50e7809
c2y3dc <ervinchow@gmail.com>
2014-11-06 18:28:33 -0800

    completed NRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -12,9 +12,39 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
+  var findSolution = function (board, n, row, verifier, callback) {
+    debugger;
+    // if all rows exhausted?
+    if(row === n){
+      // increase count
+      return callback();
+    }
+
+    for (var i = 0; i < n; i++) {
+    //iterate over a row, making each decision once
+      // place a piece
+      board.togglePiece(row, i);
+      // if !conflicts
+      if(!board[verifier]()){
+        // recurse
+        return findSolution(board, n, row + 1, verifier, callback);
+      }
+    // remove a piece
+      board.togglePiece(row, i);
+    }
+  }
 
 window.findNRooksSolution = function(n) {
+  var board = new Board({n:n});
+  var solution = board.rows();
 
+  return findSolution(board, n, 0, 'hasAnyRooksConflicts', function() {
+    // return solution
+    solution = board.rows();
+    return solution;
+  });
+
+  return solution;
 };
 
 
